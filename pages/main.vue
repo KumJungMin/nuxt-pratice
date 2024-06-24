e0d408b30037e302bb27fe2b12e246e911a27660
KumJungMin <rmawjdals@naver.com>
2024-06-24 20:48:07 +0900

    3. 기존 vue처럼 데이터 렌더링 과정은 비슷함

--- pages/main.vue
+++ pages/main.vue
@@ -3,16 +3,24 @@
   .nuxt의 router.ts에 자동으로 추가됨 -->
   <div>
     <h1>메인 페이지</h1>
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        {{ product.name }} - {{ product.price }}원
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup lang="ts">
 import axios from 'axios'
+import { ref } from 'vue'
+
+const products = ref([])
 
 async function init() {
   try {
     const res = await axios.get('http://localhost:3000/products')
-    console.log(res)
+    products.value = res.data
   } catch (err) {
     console.error(err)
   }
