1036d24e994e6f2ea62699d7755d6c4beacb6906
KumJungMin <rmawjdals@naver.com>
2024-06-24 20:25:43 +0900

    6. nuxt-link가 곧 router-link

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <!-- layout에는 공통으로 적용되어야할 UI를 넣음 -->
-    <!-- nuxt는 .nuxt로 인해 auto import됨 -->
-    <h1>{{ $route.name }} 페이지</h1>
-
-    <!-- Nuxt는 router-view와 같은 역할 -->
-    <!-- Layout이 먼저 렌더링되고, 그 하위에 pages가 그려짐 -->
+    <header>
+      <!-- nuxt-link는 페이지 이동을 위한 컴포넌트 -->
+      <h1>{{ $route.name }} 페이지</h1>
+      <nuxt-link to="/">홈</nuxt-link>
+      <nuxt-link to="/main">메인</nuxt-link>
+    </header>
     <Nuxt />
   </div>
 </template>
