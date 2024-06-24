ce2fc703062479e15a54eb730f79e5c7de8ce7be
KumJungMin <rmawjdals@naver.com>
2024-06-24 20:17:22 +0900

    4. layouts/error.vue는 공통 에러 페이지이다

---
+++ layouts/error.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- layouts/error.vue는 에러 페이지 -->
+  <div>
+    <h1>Error</h1>
+    <nuxt-link to="/">Home</nuxt-link>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
