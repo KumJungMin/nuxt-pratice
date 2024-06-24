6d0bdbc1d58830658f774e97d954f3a63aebc127
KumJungMin <rmawjdals@naver.com>
2024-06-24 20:23:21 +0900

    5. layout에는 공통으로 적용되어야할 UI를 넣음

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,13 @@
 <template>
-  <!-- Nuxt는 router-view와 같은 역할 -->
-  <!-- Layout이 먼저 렌더링되고, 그 하위에 pages가 그려짐 -->
-  <Nuxt />
+  <div>
+    <!-- layout에는 공통으로 적용되어야할 UI를 넣음 -->
+    <!-- nuxt는 .nuxt로 인해 auto import됨 -->
+    <h1>{{ $route.name }} 페이지</h1>
+
+    <!-- Nuxt는 router-view와 같은 역할 -->
+    <!-- Layout이 먼저 렌더링되고, 그 하위에 pages가 그려짐 -->
+    <Nuxt />
+  </div>
 </template>
 
 <script setup lang="ts"></script>
--- pages/main.vue
+++ pages/main.vue
@@ -2,7 +2,7 @@
   <!-- pages폴더에 파일을 추가하면, 
   .nuxt의 router.ts에 자동으로 추가됨 -->
   <div>
-    <h1>11</h1>
+    <h1>메인 페이지</h1>
   </div>
 </template>
 
